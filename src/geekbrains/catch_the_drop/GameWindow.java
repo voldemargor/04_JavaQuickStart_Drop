5a0511c87108ed5e75aed843e4a142af6c4afc9b
Vladimir Gorshkov <voldemar.gor@gmail.com>
2019-04-13 17:28:14 +0300

    The drop is falling

--- src/geekbrains/catch_the_drop/GameWindow.java
+++ src/geekbrains/catch_the_drop/GameWindow.java
@@ -1,27 +1,48 @@
 package geekbrains.catch_the_drop;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame
 {
     private static GameWindow game_window;
+    private static long last_frame_time;
+    private static Image background;
+    private static Image drop;
+    private static Image game_over;
+    private static float drop_left = 200;
+    private static float drop_top = -100;
+    private static float drop_v = 200;
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
+
         game_window = new GameWindow();
         game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         game_window.setLocation(200, 100);
         game_window.setSize(906, 478);
         game_window.setResizable(false);
+        last_frame_time = System.nanoTime();
         GameField game_filed = new GameField();
         game_window.add(game_filed);
         game_window.setVisible(true);
     }
     private static void onRepaint(Graphics g)
     {
-        g.fillOval(10, 10, 200, 100);
-        g.drawLine(130, 30, 530, 430);
+        long curren_time = System.nanoTime();
+        float delta_time = (curren_time - last_frame_time) * 0.000000001f;
+        last_frame_time = curren_time;
+
+        drop_top = drop_top + drop_v * delta_time;
+        drop_left = drop_left + drop_v * delta_time;
+        g.drawImage(background, 0, 0, null);
+        g.drawImage(drop, (int) drop_left, (int) drop_top, null);
+//        g.drawImage(game_over, 280, 120, null);
     }
     private static class GameField extends JPanel
     {
@@ -30,6 +51,7 @@ public class GameWindow extends JFrame
         {
             super.paintComponent(g);
             onRepaint(g);
+            repaint();
         }
     }
 }
